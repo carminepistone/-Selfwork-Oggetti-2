500dc8c3a789c318a600c60c6e7a64badbfa95bd
carminepistone <132765808+carminepistone@users.noreply.github.com>
2026-02-06 20:40:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+let nome = "Paola";
+let numero;
+let check = Number(prompt("premi 1 se vuoi eliminare o aggiungere un contatto, \n premi 2 se vuoi modificare un contatto"))
+
+let rubrica = {
+  'contatti': [
+      {'nome': 'Nicola', 'telefono': '3331111111'},
+      {'nome': 'Lorenzo', 'telefono': '3332222222'},
+      {'nome': 'Paola', 'telefono': '3333333333'},
+      {'nome': 'Jenny', 'telefono': '3334444444'}
+  ],
+  
+  mostraContatti : function(){
+      this.contatti.forEach(contatto => {
+          console.log(`${contatto.nome} : ${contatto.telefono}`);
+      })
+  },
+  
+  mostraContatto : function(nome){
+     let contatto = this.contatti.find(contatto => contatto.nome == nome)
+     if(contatto){
+          console.log(`Contatto Trovato - ${contatto.nome} : ${contatto.telefono}`);
+     }else{
+          console.log(`Contatto NON Trovato`);
+     }
+  },
+  
+  rimuoviAggiungiContatto : function(nome, numero){
+     let contatto = this.contatti.find(contatto => contatto.nome == nome)
+     let index = this.contatti.indexOf(contatto)
+     if(contatto){
+          this.contatti.splice(index, 1)
+          console.log(`Contatto Eliminato`);
+     }else{
+          this.contatti.push({nome: nome, telefono: numero})
+          console.log(`Contatto Aggiunto`);
+     }
+  },
+  
+  modificaContatto : function(nome, nuovoNumero){
+     let contatto = this.contatti.find(contatto => contatto.nome == nome)
+     if(contatto){
+          contatto.telefono = nuovoNumero;
+          console.log(`Contatto Modificato`);
+     }else{
+          console.log(`Contatto NON Trovato`);
+     }
+  }
+}
+
+
+rubrica.mostraContatto(nome)
+
+
+     switch (check) {
+          case 1:
+           nome = prompt("inserisci il nome del contatto")
+           numero= prompt("inserisci il numero del contatto")
+           rubrica.rimuoviAggiungiContatto(nome, numero)
+          break;
+          case 2:
+           nome = prompt("inserisci il nome del contatto")
+           numero= prompt("inserisci il numero del contatto")
+           rubrica.modificaContatto (nome, numero)   
+          break;
+     }
+
+
+
+rubrica.mostraContatti()
